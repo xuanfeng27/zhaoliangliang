8236d063645408be325df5f055a6838aa63ff2f7
zhaoliangliang <82825287@qq.com>
2017-03-21 09:29:50 +0800

    change

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                Hello World!!!ddfjjuuyydd
+                Hello World!!!
             </div>
         );
     }
